6d2a9a2add4f0de01d5a721928f5c28d1e263334
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 22:17:56 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
-        value.delete_if(|ice_cream| ice_cream == 'strawberry')
+        value.delete_if{|ice_cream| ice_cream == 'strawberry'}
       end
     end
   end
